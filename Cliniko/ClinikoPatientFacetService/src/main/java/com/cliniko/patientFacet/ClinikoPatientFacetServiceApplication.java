d9e5af0852bc04e41089c3ba15816bf4af40f120
inallib <inallib@gmal.com>
2017-09-18 16:51:20 +0530

    Eureca integration. Not communicating yet

--- Cliniko/ClinikoPatientFacetService/src/main/java/com/cliniko/patientFacet/ClinikoPatientFacetServiceApplication.java
+++ Cliniko/ClinikoPatientFacetService/src/main/java/com/cliniko/patientFacet/ClinikoPatientFacetServiceApplication.java
@@ -2,8 +2,10 @@ package com.cliniko.patientFacet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ClinikoPatientFacetServiceApplication {
 
 	public static void main(String[] args) {
--- Cliniko/ClinikoPatientFacetService/src/main/java/com/cliniko/patientFacet/service/PatientFacetService.java
+++ Cliniko/ClinikoPatientFacetService/src/main/java/com/cliniko/patientFacet/service/PatientFacetService.java
@@ -1,19 +1,28 @@
 package com.cliniko.patientFacet.service;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import com.cliniko.patientFacet.model.Patient;
 
 @Service
 public class PatientFacetService {
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@ResponseBody
 	public String save(Patient patient) {
 		return "Saved yes";
 	}
 	
-	public @ResponseBody Iterable<Patient> findAll(){
-		return null;
+	@ResponseBody
+	public Iterable<Patient> findAll(){
+		
+		return null; 
+	    
 	}
 }
