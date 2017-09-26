43e0c2f12397b726140b88473dc88131ec838099
inallib <inallib@gmal.com>
2017-09-27 00:20:12 +0530

    Eureka server discovery for web project

--- Cliniko/ClinikoWeb/src/main/java/com/service/patient/PatientWebService.java
+++ Cliniko/ClinikoWeb/src/main/java/com/service/patient/PatientWebService.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
-//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 import java.util.List;
 
@@ -19,28 +19,28 @@ import java.util.List;
 @Service
 public class PatientWebService {
 
-//    @Autowired
-//    private RestTemplate restTemplate;
-//
-//    @Bean
-//    @LoadBalanced
-//    public RestTemplate getRestTemplate(){
-//        return new RestTemplate();
-//    }
-//
-//    @ResponseBody
-//    public Iterable<Patient> findAll(){
-//
-//        ResponseEntity<List<Patient>> exchange =
-//                this.restTemplate.exchange(
-//                        "http://patient-facet/patientfacet/loadall",
-//                        HttpMethod.GET,
-//                        null,
-//                        new ParameterizedTypeReference<List<Patient>>() {
-//                        },
-//                        (Object) "mstine");
-//
-//        return exchange.getBody();
-//
-//    }
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @ResponseBody
+    public Iterable<Patient> findAll(){
+
+        ResponseEntity<List<Patient>> exchange =
+                this.restTemplate.exchange(
+                        "http://patient-facet/patientfacet/loadall",
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<Patient>>() {
+                        },
+                        (Object) "mstine");
+
+        return exchange.getBody();
+
+    }
 }
